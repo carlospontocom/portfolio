84f3572c66afd05ef19d4a7f636de098fc27c1f8
frontendcnn <frontendcnn@gmail.com>
2025-11-03 18:16:13 +0000

    reiniciar códigos

---
+++ src/GlobalStyles.jsx
@@ -0,0 +1,13 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+*{
+    padding:0;
+    margin:0; 
+    box-sizing:border-box;
+    font-size:16px;
+    font-family:sans-serif;
+    list-style: none;
+   }
+`
+
